676d1554e0871eb9c1855cd161f226f9dff71390
Sarai Munoz <saramunoz373@gmail.com>
2025-04-21 22:07:05 -0700

    updated website

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,20 +1,33 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Instructions on how to make a pb&j.
+// Author: Saraí Munoz.
+// Date: 4/20/25.
 
-// Constants
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
+ main(MakePBandJSandwich) 
+ {
+Start //Grab a paper plate
+// Grab bread package;
+// Untwist bread package;
+// Open bread bag and remove two slices;
+// Place slices on paper plate;
+// Grab a plastic knife;
+// Open peanut butter jar;
+// Use knife to scoop out peanut butter;
+// Apply peanut butter to one slice of bread;
+// Spread peanut butter on slice;
+// Place knife on plate;
+// Close peanut butter jar;
+// Open jelly bottle;
+// Squeeze jelly onto second bread slice;
+// Close jelly bottle;
+// Place down jelly;
+// Pick up knife;
+// Spread jelly on slice;
+// Bring two slices of bread together;
+// Cut slices in half down the middle;
+// Throw knife in the trash;
+// Pick up one half of sandwich;
+// Enjoy;
 }
 
 // let's get this party started
