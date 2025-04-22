e37256cc2c5b75b649bddee112435c241391e2f1
Sarai Munoz <saramunoz373@gmail.com>
2025-04-22 14:21:43 -0700

    update

---
+++ lab5/js/lab copy.js
@@ -0,0 +1,23 @@
+/* 
+* Author: Saraí Muñoz
+* Created: 22 April 2025
+* Liscense: Public Domain
+*/
+
+// Define Variables
+
+make = "Hyundai";
+model = "Accent SE";
+color = "Black"
+year = 2019;
+
+// Calculate
+age = 2025 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+//
